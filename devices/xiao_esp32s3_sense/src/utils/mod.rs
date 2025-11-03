78acedac46a883b6286a4012ece9bba6b038245c
junkei_okinawa <ceazy.x2.okinawan@gmail.com>
2025-11-03 10:27:16 +0900

    fix: Add utils module to main.rs and export DeserializeError
    - main.rs に utils モジュールを追加（streaming.rs が参照するため）
    - utils/mod.rs で DeserializeError を pub use に追加
    - コンパイルエラー E0433 を修正

--- devices/xiao_esp32s3_sense/src/utils/mod.rs
+++ devices/xiao_esp32s3_sense/src/utils/mod.rs
@@ -8,4 +8,4 @@ pub mod streaming_protocol;
 // 便利な再エクスポート
 pub use voltage_calc::calculate_voltage_percentage;
 pub use tds_calc::{calculate_tds_from_ec, compensate_ec_temperature, calculate_ec_from_adc};
-pub use streaming_protocol::{MessageType, StreamingHeader, StreamingMessage};
+pub use streaming_protocol::{MessageType, StreamingHeader, StreamingMessage, DeserializeError};
